44cdecc2adf77351337cbc928641404883f6d8f3
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-05-26 23:54:14 -0500

    added challenge 2 cat generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,12 @@ document.getElementById('flex-box-result').appendChild(h1);
 function reset() {
     document.getElementById('ageInDays').remove();
 }
+
+//Challenge 2: Cat generator
+
+function generateCat() {
+   var image = document.createElement('img');
+   var div = document.getElementById('flex-cat-gen'); 
+   image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&siza=small";
+   div.appendChild(image);
+}
