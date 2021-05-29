0766bd38e43bf51f8049e948da2246eb221de232
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-05-28 19:47:01 -0500

    html + css for blackjack challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,3 +144,35 @@ function randomColors() {
         all_buttons[i].classList.add(choices[randomNumber]);
     }
 }
+
+// Challenge 5: blackjack
+
+let blackjackGame = {
+    'you':{'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+    'dealer':{'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+};
+
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+
+const hitSound = new Audio('assets/sounds/swish.m4a');
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+function blackjackHit() {
+   showCard(YOU);
+}
+
+function showCard(activePlayer) {
+    let cardImage = document.createElement('img');
+    cardImage.src = 'assets/images/blackjack-images/Q.png';
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+function blackjackDeal() {
+let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+console.log(yourImages);
+}
